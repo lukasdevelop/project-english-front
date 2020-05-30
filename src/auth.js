e8291a866aab560154e687b069dc248d00c9a1bf
Lucas Amaral <lucasamaral@MacBook-Pro-de-Lucas.local>
2020-05-30 01:06:32 -0300

    auth react app secret

--- src/auth.js
+++ src/auth.js
@@ -2,7 +2,7 @@ import jwt, { decode } from 'jsonwebtoken'
 
 export const isAuthenticate = () => {
 
-  const secret = 'cec518b33ff1c6a616a1ced8b1635ee9'
+  const secret = process.env.REACT_APP_SECRET
   const token = localStorage.getItem('token')
 
   const result = jwt.verify(token, secret, (err, decode) => {
