8221e5ba921f4435699334451029ad5b36d8da85
Lucas Amaral <lucasamaral@MacBook-Pro-de-Lucas.local>
2020-06-03 01:29:03 -0300

    responsividade na principal

--- src/styles.js
+++ src/styles.js
@@ -1,18 +1,22 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
+ @media(max-width: 960px){
+  display: flex;
+  flex-direction: column;
+  }
   display: flex;
   justify-content: space-between;
-
   max-width: 1200px;
-  border: 1px solid red;
+
 `
 export const Title = styled.h1`
   color: #404040;
   font-size: 30px;
 `;
 export const Projects = styled.div`
-  max-width: 300px;
+
+  width: 100%;
   height: 200px;
   padding:20px;
   background: #e0e0e0;
